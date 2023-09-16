67164b6b59ea4bf6e47d217a639dfcf67384614e
Anderson Andrad <anderson_andrade_@outlook.com>
2023-09-15 21:03:04 -0300

    example: add examples of input, button, map and others

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,17 @@
-import { createBrowserRouter } from "react-router-dom";
-import { App } from "./App";
-import { Home } from "./pages/home";
+import { createBrowserRouter } from 'react-router-dom';
+import { App } from './App';
+import { Home } from './pages/home';
+import { ResultScreenExamplePage } from './pages/result-screen.example.page';
 
-export const Router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  }, {
-    path: '/home',
-    element: <Home />
-  }
-])
+export const Router = createBrowserRouter( [
+    {
+        path: '/',
+        element: <App/>
+    }, {
+        path: '/home',
+        element: <Home/>
+    }, {
+        path: '/result',
+        element: <ResultScreenExamplePage></ResultScreenExamplePage>
+    }
+] );
