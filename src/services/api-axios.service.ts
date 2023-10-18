ce03001a97cbe77e981bd8ff8a4d8b9856b10420
Anderson Andrade <anderson_andrade_@outlook.com>
2023-10-18 12:53:14 -0300

    config: change endpoint backend

--- src/services/api-axios.service.ts
+++ src/services/api-axios.service.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const axiosInstance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://match-career-nodejs-6035d87e1a9b.herokuapp.com/',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json'
